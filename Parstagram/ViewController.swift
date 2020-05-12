8d1a6d103e15f41470f2d7ae8e3152fcf8b18c2c
Parham Armani <Parham.Armani@yahoo.com>
2020-05-11 17:39:05 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Parham Armani on 5/11/20.
+//  Copyright © 2020 Parham Armani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
